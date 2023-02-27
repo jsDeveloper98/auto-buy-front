5a707a0bffef01206c506e9578b4ec97581eeeb6
Hovhannes Boyakhchyan <hovhannes.boyakhchyan@softconstruct.com>
2023-02-27 14:41:57 +0400

    some logs for debug

--- src/redux/slices/users.ts
+++ src/redux/slices/users.ts
@@ -5,7 +5,22 @@ import { RegFormValues } from "../../pages/register/Register.types";
 export const createUser = createAsyncThunk(
   "users/create",
   async (values: RegFormValues) => {
+    const data = await fetch("http://localhost:3000/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+
+    console.log("%c data ===>", "color: #90ee90", data);
     console.log("%c values ===>", "color: #90ee90", values);
+
+    console.log(
+      "%c await data.json() ===>",
+      "color: #90ee90",
+      await data.json()
+    );
   }
 );
 
